c6895dedfd58a1f81b39337b68c1dfd8fc78a457
shawn-sw <146413305+shawn-sw@users.noreply.github.com>
2024-11-11 15:29:30 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,7 @@
-// todo
+import "./style.css";
+
+const button = document.createElement("button");
+button.textContent = "Click";
+button.onclick = () => alert("You clicked the button!");
+
+button.className = "button"; 
