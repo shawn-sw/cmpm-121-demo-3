910beb7e8f6bab6cdd698d0542ac04916466e402
shawn-sw <146413305+shawn-sw@users.noreply.github.com>
2024-11-11 15:51:15 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import "./style.css";
 
+const APP_NAME = "D3";
+const app = document.querySelector<HTMLDivElement>("#app")!;
+document.title = APP_NAME;
+
 const button = document.createElement("button");
+app.append(button);
 button.textContent = "Click";
 button.onclick = () => alert("You clicked the button!");
 
